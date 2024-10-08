112d6d23474a726df9f59b76471f9817156672ed
Lucas Galton <lucas@galton.fr>
2024-10-08 07:36:10 +0200

    Update ETH library

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
-pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
+#sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
+#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
+#pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
+
+# Change in archive-build.sh awk to gawk
+brew install gsed
+brew install gawk
+brew install gperf
+brew install ninja
+brew install ccache
+python -m pip install --upgrade pip
+pip install wheel future pyelftools
